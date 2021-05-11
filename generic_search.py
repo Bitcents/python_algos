8608bf9af00a980879edbfd45b35f119264c8fa7
Bitcents <bitcentsup@gmail.com>
2021-05-12 08:59:03 +0900

    added code to generic_search.py

---
+++ generic_search.py
@@ -0,0 +1,134 @@
+from __future__ import annotations
+from typing import TypeVar, Iterable, Sequence, Generic, List, Callable, Set, Deque, Dict, Any, Optional
+from typing_extensions import Protocol
+from heapq import heappush, heappop 
+from timeit import default_timer as timer
+
+T = TypeVar('T')
+
+
+C = TypeVar("C", bound="Comparable")
+
+class Comparable(Protocol):
+    def __eq__(self: C, other: Any) -> bool:
+        ...
+    def __lt__(self: C, other: C) -> bool:
+        ...
+    def __gt__(self: C, other: C) -> bool:
+        return self < other and self != other
+    def __le__(self: C, other: C) -> bool:
+        return self < other or self == other
+    def __ge__(self: C, other: C) -> bool:
+        return not self < other
+
+
+
+# The stack data structure was defined elsewhere
+# but let us re-implement it here, just for fun :D
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        # we use lists to implement the stack
+        self._container: List[T] = []
+    
+    @property
+    def empty(self) -> bool:
+        return not self._container
+
+    def pop(self) -> T:
+        if len(self._container) > 0:
+            return self._container.pop()
+        else:
+            # do nothing if stack is empty
+            return
+
+    def push(self, item: T) -> None:
+        self._container.append(item)
+    
+    def __repr__(self) -> str:
+        # prints the stack from top to bottom
+        # a stack usually does not have a print function
+        # I include one for debugging purposes
+        output_str = None
+        if len(self._container) > 0:
+            output_str = "\n".join([str(self._container[i]) for i in range(len(self._container) - 1, 0, -1)])
+        else:
+            output_str = "stack is empty"
+        return output_str
+
+
+
+# Dfs and bfs is usually performed on a collection of nodes
+# We will define the node class here
+class Node(Generic[T]):
+    def __init__(self, state: T, parent: Optional[Node], cost: float=0.0, heuristic: float=0.0) -> None:
+        self._state: T = state
+        self._cost: float = cost
+        self._heuristic: float = heuristic
+        self._parent: Optional[Node] = parent
+
+    def __lt__(self, other: Node) -> bool:
+        return(self._cost + self._heuristic < (other._cost + other._heuristic))
+    
+
+
+# Binary search depends on the sequence being sorted
+# It will not work on unsorted lists, for example
+def binary_contains(sequence: Sequence[C], key: C) -> bool:
+    low: int = 0
+    high: int = len(sequence) - 1
+
+    while low <= high:
+        mid: int = (low + high) // 2
+        if sequence[mid] < key:
+            high = mid - 1
+        elif sequence[mid] > key:
+            left = mid + 1
+        else:
+            return True
+    return False
+
+# The simplest way to search for something
+# This will work whether the sequence in question is sorted or not
+def linear_contains(iterable: Iterable[T], key: T) -> bool:
+    for item in iterable:
+        if item == key:
+            return True
+    
+    return False
+
+
+
+
+# perform some tests and make some comparisons
+
+if __name__ == "__main__":
+    list = [1, 5, 15, 15, 15, 15, 20, 21, 22, 25, 30, 31, 32, 50, 51, 100, 121, 122, 123, 124, 137]
+    
+    s_time_1 = timer()
+    linear_test = linear_contains(list, 30)
+    e_time_1 = timer()
+    
+    s_time_2 = timer()
+    binary_test = binary_contains(list, 30)
+    e_time_2 = timer()
+
+
+    print(f'linear test result: {linear_test}. Time taken: {e_time_1 - s_time_1}')
+    print(f'binary test result: {binary_test}. Time taken: {e_time_2 - s_time_2}')
+
+    # print(binary_contains(["a", "d", "e", "f", "z"], "f")) # True
+    # print(binary_contains(["john", "mark", "ronald", "sarah"], "sheila")) #False  
+
+    # It should be clear from the results that for short sequences
+    # Linear searching would be faster than binary searching
+    # However, once the data set grows large enough,
+    # Binary searching would be much faster
+
+        # stack tests
+    test_list = [1,2,3,4,5,6,7,8,9]
+    test_stack = Stack()
+    for item in test_list:
+        test_stack.push(item)
+    top_item = test_stack.pop()
+    print(test_stack)

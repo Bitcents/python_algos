6f9c5095904e7f26548d75ff893484fbd51a80a4
Bitcents <bitcentsup@gmail.com>
2021-05-16 19:09:27 +0900

    added CSP class to csp file

--- csp.py
+++ csp.py
@@ -14,4 +14,20 @@ class Constraint(Generic[V, D], ABC):
     @abstractmethod
     def satisfied(self, assignment: Dict[V, D]) -> bool:
         ...
-    
+
+class CSP(Generic[V, D]):
+    def __init__(self, variables: List[V], domains: Dict[V, D]) -> None:
+        self.variables: List[V]= variables
+        self.domains: Dict[V, List[D]] = domains
+        self.constraints: Dict[V, List[Constraint[V, D]]] = {}
+        for variable in self.variables:
+            self.constraints[variable] = []
+            if variable not in self.domains:
+                raise LookupError("Every variable must have an associated domain")
+
+    def add_constraint(self, constraint: Constraint[V, D]):
+        for variable in constraint.variables:
+            if variable not in self.variables:
+                raise LookupError("Variable in constraint not present in CSP")
+            else:
+                self.constraints[variable].append(constraint)

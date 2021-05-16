1cab47ea216aed784f7501d81f75b667e3785935
Bitcents <bitcentsup@gmail.com>
2021-05-16 16:40:51 +0900

    edited the practice section

--- practice/practice.py
+++ practice/practice.py
@@ -1,9 +1,4 @@
-# This is the file where I try to rewrite all the other code in this project
-# This should eventually become the biggest file
-# I will put references where necessary
-
-
-from typing import Generic, Optional, TypeVar, List, Set, Callable, Deque
+from typing import Generic, Optional, TypeVar, List, Set, Callable, Deque, Dict
 from typing_extensions import Protocol
 from heapq import heappush, heappop
 from __future__ import annotations
@@ -26,6 +21,10 @@ class Stack(Generic[T]):
         # we will use Python lists to implement a stack
         self._container: List[T] = []
     
+    @property
+    def is_empty(self) -> bool:
+        return self._container == None 
+
     def pop(self) -> T:
         self._container.pop()
     
@@ -40,6 +39,10 @@ class Queue(Generic[T]):
     def __init__(self) -> None:
         self._container: Deque[T] = Deque()
     
+    @property
+    def is_empty(self) -> bool:
+        return self._container == None
+
     def pop(self) -> T:
         self._container.popleft()
     
@@ -54,6 +57,10 @@ class Queue(Generic[T]):
 class PriorityQueue(Generic[T]):
     def __init__(self):
         self._container: List[T] = []
+
+    @property
+    def is_empty(self):
+        return self._container == None
 
     def pop(self) -> T:
         return heappop(self._container)
@@ -75,3 +82,60 @@ class Node(Generic[T]):
     def __lt__(self, other: Node) -> bool:
         return (self.cost + self.heuristic) < (other.cost + other.heuristic)
 
+
+def dfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]]) -> Optional(Node[T]):
+    # initlalize stack and populate with initial node
+    frontier: Stack[Node[T]] = Stack()
+    frontier.push(Node(initial, None))
+    # initialze set of explored nodes
+    explored: Set[T] = Set()
+    explored.add(T)
+    # loop until frontier is not empty:
+    while not frontier.is_empty:
+        current_node = frontier.pop()
+        current_state = current_node.state
+        if goal_test(current_state):
+            return current_node
+        for child in successors(current_state):
+            if child in explored:
+                continue
+            else:
+                frontier.push(child)
+                explored.add(child)
+
+
+def bfs(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]]) -> Optional(Node[T]):
+    # intialize queue and populate with initial node
+    frontier: Queue[Node[T]] = Queue()
+    frontier.push(Node[T], None)
+    # initialze set of explored nodes
+    explored: Set[T] = Set()
+    explored.add(T)
+    # loop while frontier is not empty
+    while not frontier.is_empty:
+        current_node = frontier.pop()
+        current_state = current_node.state
+        if goal_test(current_state):
+            return current_node
+        for child in successors(current_state):
+            if child in explored:
+                continue
+            else:
+                frontier.push(child)
+                explored.add(child)
+
+
+def astar(initial: T, goal_test: Callable[[T], bool], successors: Callable[[T], List[T]], heuristic: Callable[[T], float]) -> Optional(Node[T]):
+    frontier: PriorityQueue[Node[T]] = PriorityQueue()
+    frontier.push(Node[initial, None, 0.0, heuristic(initial)])
+    explored: Dict[T, float] = {initial : 0.0}
+    while not frontier.is_empty:
+        current_node = frontier.pop()
+        current_state = current_node.state
+        if goal_test(current_state):
+            return current_node
+        for child in successors(current_state):
+            new_cost = current_node.cost + 1
+            if not child in explored or explored[child] > new_cost:
+                explored[child] = new_cost
+                frontier.push(Node(child, current_node, new_cost, heuristic(child))) 

d08aa25e3f8a0ba416976b8afe1f9cfde7a5af91
Bitcents <bitcentsup@gmail.com>
2021-05-20 22:04:21 +0900

    started working on kmeans clustering

---
+++ kmeans.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from typing import TypeVar, Generic, List, Sequence
+from copy import deepcopy
+from functools import partial
+from random import uniform
+from statistics import mean, pstdev
+from dataclasses import dataclass
+from data_point import DataPoint
+
+
+def zscores(original: Sequence[float]) -> List[float]:
+    avg: float = mean(original)
+    std: float = pstdev(original)
+    if std == 0:
+        return [0]
+    return [(x - avg)/std for x in original]
+
--- sorting.py
+++ sorting.py
@@ -50,25 +50,38 @@ def quick_sort(numbers: List[int], low: int, high: int) -> None:
         quick_sort(numbers,low, i)
         quick_sort(numbers, i + 1, high)
 
+
+
+
+# We need an easy way to make large lists of integers, for testing our sorting algorithms
+# This function helps us create such lists easily
 def create_random_integers(lower: int, upper: int, no_of_ints) -> List[int]:
     arr = []
     for _ in range(no_of_ints):
         arr.append(random.randint(lower, upper))
     return arr
 
+
+
 if __name__=='__main__':
-    test_numbers = create_random_integers(0,50,200)
+
+    # Due to how Python works, we must make copies of the lists we create
+    # With the copy method
+    test_numbers = create_random_integers(0,50,1000)
     test_numbers_copy = test_numbers.copy()
+    
+    # Test algorithms
     s = timer()
     quick_sort(test_numbers, 0, len(test_numbers))
     e = timer()
     difference1 = e - s
-    print(test_numbers)
+    #print(test_numbers)
 
     s = timer()
+
     insertion_sort(test_numbers_copy)
     e = timer()
     difference2 = e - s
-    print(test_numbers)
-    print(f'bubble_sort time: {difference1}, insertion_sort time = {difference2}')
+    #print(test_numbers)
+    print(f'quick_sort time: {difference1}, insertion_sort time = {difference2}')
     
